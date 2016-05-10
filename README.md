a2a4df8406bb1904cba8fef7d9b7d0931e855f22
szydan <danielczyk.szymon@gmail.com>
2016-05-10 16:00:53 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Custom Kibi Icons
+=================
+
+Made thanks to awesome project [fontcustom](https://github.com/FontCustom/fontcustom) 
+
+#### To regenerate:    
+ 
+* add your vector *.svg file to **vectors** folder
+* follow the instructions at [fontcustom](https://github.com/FontCustom/)
+
+```
+fontcustom compile -F
+fontcustom watch vectors
+```
+
---
+++ bower.js
@@ -0,0 +1,23 @@
+{
+  "name": "kibi-icons",
+  "description": "Custom Kibi Icons",
+  "keywords": [],
+  "dependencies": {},
+  "devDependencies": {},
+  "license": [
+    "MIT"
+  ],
+  "main": [
+    "lib/styleshets/FontAwesomeKibi.css"
+  ],
+  "ignore": [
+    "*/.*",
+    "*.json",
+    "src",
+    "*.yml",
+    "Gemfile",
+    "Gemfile.lock",
+    "*.md"
+  ],
+  "version": "1.0.2"
+}
